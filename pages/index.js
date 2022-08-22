d4cad0b30947216e6e1652216133d992972ab28b
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-08-22 19:41:36 +0530

    user Schema

--- pages/index.js
+++ pages/index.js
@@ -67,3 +67,4 @@ export default function Home() {
     </div>
   )
 }
+
--- sanitystudio/schemas/schema.js
+++ sanitystudio/schemas/schema.js
@@ -3,7 +3,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-
+import { account, verificationToken } from 'next-auth-sanity/schemas';
+import user from './user';
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -11,6 +12,6 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    user, account, verificationToken
   ]),
 })
