45fe4eeeab46e4ba1df2b7bd659240dbdd97f52c
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-08-28 21:39:39 +0530

    prisma warning fix

--- pages/api/create/team.js
+++ pages/api/create/team.js
@@ -1,9 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { PrismaClient } from "@prisma/client";
 import { unstable_getServerSession } from "next-auth"
 import { authOptions } from "../auth/[...nextauth]"
-const prisma = new PrismaClient();
+import prisma from "../../../libs/prisma"
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
