35dd3ddf72c3cd5f21c442fc3615de13654aa447
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-08-28 21:44:34 +0530

    prisma warning fix

--- libs/prisma.js
+++ libs/prisma.js
@@ -1,3 +1,10 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client';
+
+const prisma = global.prisma ||
+    new PrismaClient({
+        log: ['query'],
+    });
+
 export default prisma;
+if (process.env.NODE_ENV !== 'production')
+    global.prisma = prisma;
