27211ce42f20ba88c993f2dc45dd7e57eb57ffb4
nagarajpandith <nagaraj.pandith2002@gmail.com>
2022-09-29 17:01:05 +0530

    profile page

--- components/hero/index.js
+++ components/hero/index.js
@@ -1,40 +1,39 @@
-import { useSession } from "next-auth/react"
-import Link from "next/link"
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 import Button from '../button';
 
 export default function Hero() {
-const { data, status } = useSession()
+  const { data, status } = useSession();
   return (
     <section className="text-black dark:text-white bg-white dark:bg-gray-900/10 transition-colors duration-500">
-    <div className="px-4 py-32 mx-auto max-w-screen-xl lg:items-center lg:flex">
+      <div className="px-4 py-32 mx-auto max-w-screen-xl lg:items-center lg:flex">
         <div className="max-w-xl mx-auto text-center">
-        <h1 className="text-3xl font-extrabold sm:text-5xl">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
             Welcome to
-            <span className="block w-full py-2 heading">
-            Finite Loop Club!
-            </span>
-        </h1>
-        
-        <p className="mt-4 sm:leading-relaxed sm:text-xl">
-            We are a Coding club of NMAMIT aiming at Realizing the idea and Inspiring the next!
-        </p>
-        <div className="flex flex-wrap justify-center mt-8">
-            {status != 'authenticated'?(
-            <Link href="/api/auth/signin">
+            <span className="block w-full py-2 heading">Finite Loop Club!</span>
+          </h1>
+
+          <p className="mt-4 sm:leading-relaxed sm:text-xl">
+            We are a Coding club of NMAMIT aiming at Realizing the idea and
+            Inspiring the next!
+          </p>
+          <div className="flex flex-wrap justify-center mt-8">
+            {status != 'authenticated' ? (
+              <Link href="/api/auth/signin">
                 <a className="block w-auto px-12 py-3 font-medium text-white bg-yellow-400 rounded shadow active:bg-yellow-500 hover:bg-yellow-300 focus:outline-none focus:ring duration-300">
-                    Sign In
+                  Sign In
                 </a>
-            </Link>
-            ):(
-            <Link href='/events'>
+              </Link>
+            ) : (
+              <Link href="/events">
                 <a className="block bg-gray-50 w-auto px-12 py-3 font-medium text-black rounded shadow hover:text-gray-400 active:text-yellow-500 focus:outline-none focus:ring duration-300">
-                    Explore Events
+                  Explore Events
                 </a>
-            </Link>
+              </Link>
             )}
+          </div>
         </div>
-        </div>
-    </div>
+      </div>
     </section>
-  )
+  );
 }
--- pages/profile.js
+++ pages/profile.js
@@ -1,24 +1,48 @@
-import React from 'react'
-import Image from 'next/image'
+import Image from 'next/image';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import Button from '../components/button';
+import { useState, useEffect } from 'react';
 
 export default function Profile() {
   const { data, status } = useSession();
+  const [profile1, setProfile] = useState([]);
+
+  const fetchProfile = async () => {
+    const res = await fetch('/api/read/users');
+    const profile = await res.json();
+    setProfile(profile);
+  };
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
   return (
     <div>
-        {status != 'authenticated'? (
-        <div className='flex flex-col justify-center items-center gap-6 m-24 text-center lg:m-56'>
-            <h1 className='text-lg lg:text-2xl'>You are not authorised, Please sign in.</h1>
-            <Button><Link href="/api/auth/signin">Sign In</Link></Button>
+      {console.log(profile1.data[1])}
+      {status != 'authenticated' ? (
+        <div className="flex flex-col justify-center items-center gap-6 m-24 text-center lg:m-56">
+          <h1 className="text-lg lg:text-2xl">
+            You are not authorised, Please sign in.
+          </h1>
+          <Button>
+            <Link href="/api/auth/signin">Sign In</Link>
+          </Button>
         </div>
-        ) : (
-        <div>
-        <Image src={data.user.image} width={100} height={100} />
-        <a>{data.user.name}</a>
+      ) : (
+        <div className="flex justify-center items-center flex-col gap-5 my-10">
+          <Image
+            className="rounded-lg"
+            src={data.user.image}
+            width={200}
+            height={200}
+          />
+          <a className="heading text-2xl font-bold">{data.user.name}</a>
+          {/* <p className='font-bold text-gray-300'>Bio: {profile.data[0].bio}</p>
+        <p className='font-bold text-gray-300'>Role: {profile.data[0].role}</p> */}
         </div>
-        )}
+      )}
     </div>
-  )
+  );
 }
