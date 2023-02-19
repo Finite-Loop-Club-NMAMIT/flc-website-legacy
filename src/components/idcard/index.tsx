b01c268407c225dc49fe69f502b93e1fe52bd472
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-02-19 23:12:37 +0530

    refactor(idcard): collapsible, minimalistic ID card

--- src/components/idcard/index.tsx
+++ src/components/idcard/index.tsx
@@ -1,109 +1,134 @@
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { api } from "../../utils/api";
-import { useQRCode } from "next-qrcode";
+import { useState, useRef, type FunctionComponent } from "react";
+import Image from "next/image";
+import { BsDownload, BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+import { QRCodeSVG } from "qrcode.react";
 import { useReactToPrint } from "react-to-print";
-import { useRef } from "react";
-import { useTheme } from "next-themes";
-import { FiDownload } from "react-icons/fi";
 
-export default function IdCard() {
-  const router = useRouter();
-  const { data, status } = useSession();
-  const username = router.query.username;
-  const user = api.userRouter.getUserByEmail.useQuery(
-    { email: data?.user?.email as string },
-    {
-      enabled: (data?.user?.email as string) !== undefined,
-    }
-  );
-  const { Image } = useQRCode();
-  const { theme } = useTheme();
-  const idRef = useRef<HTMLDivElement>(null);
-  const printPdf = useReactToPrint({
-    content: () => (idRef.current ? idRef.current : null),
-    pageStyle: `@page {margin:-150px 0 0 0; size: 480px 640px !important;`,
+type IDCardProps = {
+  image: string;
+  name: string;
+  username: string;
+  role: string;
+  email: string;
+};
+
+const IDCard: FunctionComponent<IDCardProps> = ({
+  image,
+  name,
+  username,
+  role,
+  email,
+}) => {
+  const [showIdCard, setShowIdCard] = useState(false);
+  const printRef = useRef(null);
+
+  const toggleIdCard = () => {
+    setShowIdCard(!showIdCard);
+  };
+
+  const handlePrint = useReactToPrint({
+    content: () => printRef.current,
+    pageStyle: `@page {margin:-150px 0 0 0; size: 350px 530px !important;`,
     copyStyles: true,
   });
 
-  const handlePrint = () => {
-    if (idRef.current) printPdf();
+  const handleDownload = () => {
+    if (printRef.current) handlePrint();
   };
 
-  if (status === "loading") return <></>;
-  if (status === "unauthenticated") return <></>;
-  if (!user || !user.data || username !== user.data.username) return <></>;
   return (
-    <section
-      className="my-9 flex flex-col items-center gap-4 py-7 px-2"
-      style={{
-        WebkitPrintColorAdjust: "exact",
-      }}
+    <div
+      className={`flex flex-col justify-between rounded-md border-2 border-gray-300 shadow-md transition-all duration-500 ease-in-out ${
+        showIdCard ? "w-full sm:w-96" : "w-72"
+      }`}
     >
-      <h3 className="heading text-2xl font-bold">ID CARD</h3>
-      <button onClick={handlePrint} className="text-yellow-400 hover:scale-110">
-        <FiDownload size={30}></FiDownload>
-      </button>
-      <div className="w-[480px] h-[1px] bg-gradient-to-r from-yellow-300 to-orange-600 -mb-4 " ></div>
-      <div
-        ref={idRef}
-        className="before:opacity-85 relative flex h-[640px] w-[480px] flex-col items-center gap-4 p-4 shadow-lg shadow-gray-300 before:absolute before:left-0 before:top-0 before:z-[-1] before:h-full before:w-full before:bg-[url('/assets/Matrix.svg')] before:blur-[1px] dark:shadow-yellow-900"
-        style={{
-          WebkitPrintColorAdjust: "exact",
-          colorAdjust: "exact",
-          printColorAdjust: "exact",
-        }}
-      >
-        <div className="flex w-full items-center gap-3 px-3">
-          <img src={`/assets/flc_logo_crop.png`} className="h-16 w-16"></img>
-          <div className="text-3xl font-bold text-yellow-600">FLC</div>
-          <div className="flex-1 text-right font-mono text-lg font-semibold text-[#FFA500]">
-            2022-23
-          </div>
-        </div>
+      <div className="flex items-center justify-between p-4">
         <div
-          className="relative h-52 w-52 overflow-hidden rounded-full p-1 
-                before:absolute before:bottom-[50%] before:left-[50%] before:z-[-1] before:h-[calc(100%)] before:w-[calc(100%)] before:origin-bottom-left
-                before:bg-gradient-to-r before:from-[#fff700] before:to-[#ff7d4d]
-                after:absolute after:top-0 after:left-0 after:z-[-2]
-                after:h-full after:w-full after:bg-[#fff700]  before:motion-safe:animate-[spin_10s_ease-in-out_infinite]"
+          className="cursor-pointer font-bold text-yellow-500"
+          onClick={toggleIdCard}
         >
-          <img
-            src={`${data?.user?.image}`}
-            className="w-full rounded-full transition-all duration-700"
-          ></img>
+          Digital ID Card
         </div>
-        <div
-          className={`relative text-2xl font-bold text-yellow-400 before:absolute before:top-1 before:left-1 before:z-[-1] before:h-full before:w-full before:text-orange-200 before:opacity-80 before:blur-[3px] before:content-['${user.data.name?.replace(
-            " ",
-            "_"
-          )}'] before:text-2xl`}
-        >
-          {user?.data?.name}
+        <div className="flex flex-row items-center">
+          <button
+            className="mx-2 hover:text-yellow-500 focus:outline-none"
+            onClick={toggleIdCard}
+          >
+            {showIdCard ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+          </button>
+          {showIdCard && (
+            <button
+              className="mx-2 hover:text-yellow-500 focus:outline-none"
+              onClick={handleDownload}
+            >
+              <BsDownload />
+            </button>
+          )}
         </div>
-        <div
-          className="text-ylw rounded-full border-4 border-yellow-300 border-b-yellow-400 p-2 text-sm font-bold uppercase text-yellow-500 shadow-md
-                shadow-yellow-300"
-        >
-          {user?.data?.isMember ? user.data.role : "Unofficial-Member"}
-        </div>
-        <Image
-          text={user.data.id}
-          options={{
-            width: 140,
-            level: "H",
-            margin: 1,
-            color: {
-              dark: "#833200",
-              light: theme === "light" ? "#ffffff00" : "#ffffffff",
-            },
-          }}
-        />
-        {/* <div className="flex w-full justify-center gap-3 items-center"> 
-                <img src={`/assets/flc_logo_crop.png`} className="w-10 h-10"></img>
-                <img src={`https://nmamit.nitte.edu.in/img/nitte-mobile-logo.png`} className="w-72 h-10"></img>
-                </div> */}
       </div>
-    </section>
+      {showIdCard && (
+        <>
+          <hr />
+          <div className="p-4" ref={printRef}>
+            <div>
+              <div className="flex w-full items-center gap-3 px-3">
+                <Image
+                  src="/assets/flc_logo_crop.png"
+                  width={50}
+                  height={50}
+                  alt="logo"
+                />
+                <div className="text-xl font-bold text-yellow-500">FLC</div>
+                <div className="text-md flex-1 text-right font-semibold">
+                  {`${
+                    new Date().getFullYear() - 1
+                  } - ${new Date().getFullYear()}`}
+                </div>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <Image
+                  src={image}
+                  width={128}
+                  height={128}
+                  alt="profile"
+                  className="rounded-full border-2 border-gray-500 dark:border-gray-300"
+                />
+                <div className="mt-5 text-xl font-bold">{name}</div>
+                <div className="font-thin">
+                  {email.startsWith("4nm")
+                    ? email.split("@")[0]?.toUpperCase()
+                    : ""}
+                </div>
+                <div className="mt-2 rounded-full border border-yellow-500 px-3 uppercase">
+                  {role}
+                </div>
+              </div>
+              <div className="m-4 flex justify-center">
+                <QRCodeSVG
+                  value={`https://finiteloop.co.in/u/${username}`}
+                  size={100}
+                  includeMargin
+                />
+              </div>
+              <hr />
+              <div className="my-3 text-center">
+                <div className="text-xs font-thin">
+                  <strong>
+                    Finite Loop Club, NMAM Institute of Technology
+                  </strong>
+                  <br />
+                  Nitte, Karkala Taluk, Udupi - 574110
+                  <br />
+                  Karnataka, India
+                  <br />
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
-}
+};
+
+export default IDCard;
--- src/pages/u/[username].tsx
+++ src/pages/u/[username].tsx
@@ -1,12 +1,10 @@
 import { type NextPage } from "next";
-import IdCard from "../../components/idcard";
 import Profile from "../../components/profile";
 
 const User: NextPage = () => {
   return (
     <>
       <Profile />
-      <IdCard/>
     </>
   );
 };
