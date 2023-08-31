7380bfb15e525b97cf7e55b8d142000493d59c51
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-08-31 14:27:20 +0530

    fix: year and name

--- src/utils/details.ts
+++ src/utils/details.ts
@@ -2,6 +2,6 @@ export function extractStudentDetailsFromEmail(email: string) {
   const batch = parseInt(email.slice(3, 5)) + 2000 + 4;
   const usn = email.split("@")[0] as string;
   const branch = usn.slice(5, 7);
-  const year = batch - 2000;
+  const year = batch - 2000 - 4;
   return { batch, branch, usn, year };
 }
