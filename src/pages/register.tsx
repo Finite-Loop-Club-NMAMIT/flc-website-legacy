632e53c9ee6a94d07c1bb8233d230e0d57d32800
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-12-05 16:37:38 +0530

    feat: open reg

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,29 +1,29 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-function Register() {
-  // Closed
-  return (
-    <section>
-      <div className="mx-auto  max-w-screen-sm  text-center">
-        <h1 className="gradient mb-4 text-4xl font-extrabold tracking-tight text-yellow-500 dark:text-yellow-500">
-          Oops Registration Closed :(
-        </h1>
-        <p className="mb-4 text-gray-500 dark:text-gray-400">
-          Hey! registration are closed now! You can{" "}
-          <Link
-            className="cursor-pointer text-yellow-500 "
-            href={"/contact-us"}
-          >
-            contact us
-          </Link>{" "}
-          for any further clarifications
-        </p>
-      </div>
-    </section>
-  );
-}
+// function Register() {
+//   // Closed
+//   return (
+//     <section>
+//       <div className="mx-auto  max-w-screen-sm  text-center">
+//         <h1 className="gradient mb-4 text-4xl font-extrabold tracking-tight text-yellow-500 dark:text-yellow-500">
+//           Oops Registration Closed :(
+//         </h1>
+//         <p className="mb-4 text-gray-500 dark:text-gray-400">
+//           Hey! registration are closed now! You can{" "}
+//           <Link
+//             className="cursor-pointer text-yellow-500 "
+//             href={"/contact-us"}
+//           >
+//             contact us
+//           </Link>{" "}
+//           for any further clarifications
+//         </p>
+//       </div>
+//     </section>
+//   );
+// }
 
-export default Register;
+// export default Register;
 
 // import Button from "../components/button";
 // import { useState } from "react";
